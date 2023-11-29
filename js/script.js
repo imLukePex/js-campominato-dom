6b6a5924b353e58254ca2538a9cbd75277b6521e
imLukePex <luca.pessione@gmail.com>
2023-11-29 18:22:09 +0100

    finalizing function and cicle bomb

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // L’utente clicca su un bottone che genererà una griglia di gioco quadrata. Ogni cella ha un numero progressivo, da 1 a 100. Ci saranno quindi 10 caselle per ognuna delle 10 righe. Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali. In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle. La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe). Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
 // selezione del'elemento contenitore
 const gridElement = document.getElementById("grid");
 
@@ -26,14 +29,17 @@ for (let i = 1; i <= 100; i++) {
                 myElement.addEventListener("click",
 
                     function() {
-                        if(newArrayNum.includes(myElement(i))) {
+                        if(newArrayNum.includes(i)) {
+                            myElement.classList.add("clicked");
                             myElement.classList.add("square-bomb");
-                            console.log("Hai selezionato una bomba!!!");
+                            console.log(`Il numero selezionato è: ${i}`);
+                            console.log("Hai selezionato una bomba, hai perso!!!");
                         } else {
+                            myElement.classList.add("clicked");
                             myElement.classList.add("square-free");
+                            console.log(`Il numero selezionato è: ${i}`);
+                            console.log("Complimenti, non sei atterrato su una bomba!");
                         }
-
-                        console.log(`Il numero selezionato è: ${i}`);
                     }
                 );
         }
